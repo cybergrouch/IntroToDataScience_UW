25a5657db7d348c65ab5d327a99bb3bf5bb40d27
cybergrouch <cyber_grouch_lange@yahoo.ie>
2014-07-12 13:37:01 +0800

    Problem 1 Solution Clean up
    Removed unnecessary codes
    
    Signed-off-by: cybergrouch <cyber_grouch_lange@yahoo.ie>

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -15,26 +15,26 @@ def hw():
     #print 'Hello, world!'
     
     tweet_file = open(sys.argv[2])
-    count = 1
+    #count = 1
     for line in tweet_file:
         json_line = json.loads(line.lower())
         if "text" in json_line:
             words = re.findall(r"[\w']+|[.,!?;]", json_line.get("text"))
             total_score = 0
         
-            line_score = {}
-            line_score["text"] = json_line.get("text")
-            line_score["words"] = {}
+            #line_score = {}
+            #line_score["text"] = json_line.get("text")
+            #line_score["words"] = {}
             for word in words:
                 if word in scores:
-                    line_score["words"][word] = scores.get(word)
+                    #line_score["words"][word] = scores.get(word)
                     total_score = total_score + scores.get(word)
-                else:
-                    line_score["words"][word] = 0
+                #else:
+                    #line_score["words"][word] = 0
             
-            line_score["line"] = count
-            line_score["total_score"] = total_score
-            count = count + 1
+            #line_score["line"] = count
+            #line_score["total_score"] = total_score
+            #count = count + 1
                 
             #print json.dumps(line_score, indent=4, separators=(',', ': '))
             print total_score
@@ -47,10 +47,10 @@ def lines(fp):
 
 def main():
     hw()
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    lines(sent_file)
-    lines(tweet_file)
+    #sent_file = open(sys.argv[1])
+    #tweet_file = open(sys.argv[2])
+    #lines(sent_file)
+    #lines(tweet_file)
 
 if __name__ == '__main__':
     main()
