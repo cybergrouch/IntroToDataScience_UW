a91daf866a1ef1ca44645883816fe285a85027ab
cybergrouch <cyber_grouch_lange@yahoo.ie>
2014-07-16 01:01:18 +0800

    Assignment 2. Problem 1.d. solution.

--- assignment2/union.sql
+++ assignment2/union.sql
@@ -1,6 +1,4 @@
 select count(*)
 from (
-    select term from frequency where docid = '10398_txt_earn' and count = 1
-    union
-    select term from frequency where docid = '925_txt_trade' and count = 1    
+    select distinct docid from frequency where term = 'parliament'
 );
